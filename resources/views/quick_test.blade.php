d60f83934aa2eedf51e5f9f2119138f5a9a2fb3b
Pamal-Ranasinghe <68419474+Pamal-Ranasinghe@users.noreply.github.com>
2021-04-03 15:30:38 +0530

    few changes added

--- resources/views/quick_test.blade.php
+++ resources/views/quick_test.blade.php
@@ -328,13 +328,13 @@
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Results</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-              ...
+              Results will appear here
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
